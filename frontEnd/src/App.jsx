d1bb1bfc1a150cd2ea02f6fe465dcca36750306c
arthur-piau <arthur.piau@3wa.io>
2023-12-12 16:48:09 +0000

    tweaks

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home.jsx"
